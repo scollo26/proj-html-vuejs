eddd8f2104df5e7d97e303239c5016dce1d57eb9
scollo26 <giovanniscollo26@gmail.com>
2022-01-24 18:11:13 +0100

    inizio main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,36 @@
 <template>
 
-<div class="header-top container-fluid"> 
-    <div class="container">
-        <div class="row row-col-2">
-            <div class="col">
-                <i class="fas fa-clock pt-3"></i>
-                <span>Open Hours: Mon-Sat- 9:00-18:00</span>
+<div class=" container-100 ">
+    <div class="header-top color-top">
+        <div class="container">
+            <div class="row row-col-2 ">
+                <div class="col">
+                    <i class="fas fa-clock pt-3"></i>
+                    <span>Open Hours: Mon-Sat- 9:00-18:00</span>
                 
-            </div>
-            <div class="col">
-                <i class="fas fa-phone-alt"></i>
-                <span>+1(305) 1234-5678</span>
-                <span><i class="fas fa-envelope"></i> hello@example.com</span>
-                <span class="icon-top">
-                    <span><i class="fab fa-facebook-f"></i></span>
-                    <span><i class="fab fa-twitter"></i></span>
-                    <span><i class="fab fa-linkedin-in"></i></span>
-                </span>
-                
-
+                </div>
+                <div class="col">
+                    <i class="fas fa-phone-alt"></i>
+                    <span>+1(305) 1234-5678</span>
+                    <span><i class="fas fa-envelope"></i> hello@example.com</span>
+                    <span class="icon-top">
+                        <span><i class="fab fa-facebook-f"></i></span>
+                        <span><i class="fab fa-twitter"></i></span>
+                        <span><i class="fab fa-linkedin-in"></i></span>
+                    </span>
+                </div>
             </div>
         </div>
-        <div class="container mt-2">
-            <div class="d-flex bd-highlight">
+    </div> 
+    
+        <div class="container ">
+            <div  class="d-flex bd-highlight">
                 <div class="p-2 flex-grow-1 bd-highlight">
                     <span class="green fs-2">NEX</span>
                     <span class="black fs-2">GEN</span>    
                 </div>
             
-                <div class="p-2 bd-highlight">
+                <div id="" class="p-2 bd-highlight">
                     <ul>
                         <li><a href="http://"> HOME</a> </li>
                         <li><a href="http://"> SERVICES</a></li>
@@ -43,23 +45,148 @@
         </div>
         
 
-        </div>
+    
         <div class="jumbo">
             <div class="jumbo-img">
-                <img src="../assets/img/bg-parallax.png" alt="">
-                
-            </div>
-            <div class="banner-jumbo text-center">
-                    <h1 class="black fs-1 fw-bold">Ready</h1>
+                <!-- <img src="../assets/img/bg-parallax.png" alt=""> -->
+                <div class="banner-jumbo text-center">
+                    <span class="black fs-1 fw-bold">Ready</span>
                     <span class="green fs-1 fw-bold">Team</span> 
                     <p>No matter what your company needs, we will be </p> 
                     <p>ready to assist you in the best possible way.</p>
                     <button class="btn btn-outline-secondary ms-3 " type="submit">GET IN TOUCH</button>
-                    <button type="button" class="btn btn-outline-success green ms-3">READ MORE</button>
+                    <button type="button" class="btn btn-outline-success  green ms-3">READ MORE</button>
                 </div>
+        
+            </div>  
+        </div> 
+        <div class="header-bottom pt-5 ">   
+            <div class="container">
+                <p class="green"> OUR BUSINESS AREAS</p>
+                <h2 class="black fw-bold"> Excellence in <span class="green">Service</span></h2>
+                <div class="row row-col-2 ">
+                    <div class="col">
+                        <p>we are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowed professionals.These are some options that you can hire.</p>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <button type="button height: 10px " class="btn btn-outline-success  green ">SEE ALL</button>
+                    </div>
+                </div>
+                <div class="row row-cols-3 pb-5" >
+                    <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                                <i class="fas fa-network-wired fs-1 green "></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                                
+                                <i class="fas fa-briefcase fs-1 green"></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                                <i class="far fa-chart-bar fs-1 green"></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                     <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                    
+                                <i class="fas fa-plane fs-1 green"></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                     <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                                
+                                <i class="fas fa-globe-americas fs-1 green"></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                     <div class="col">
+                        <div class="business">
+                            <div class="d-flex justify-content-between p-4">
+                                
+                                <i class="fas fa-inbox fs-1 green"></i>
+                            <div class=" arrow p-3 ">
+                                
+                                <i class="fas fa-arrow-right fs-4 "></i>
+                            </div>
+                            </div>
+                            
+                            <h2 class="black ps-3">Audit & Assurance</h2>
+                            <p class="ps-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            
+                            
+                        </div>
+                    </div>
+                    
+
+                </div>
+                
+                
+
+            </div>
             
-    </div> 
+    </div>
+    
+    
+    
 </div>
+
 </template>
 
 <script>
@@ -69,13 +196,14 @@ export default {
 </script>
 
 <style lang="scss">
-.container-fluid{
-    background-color: #122439;
+.container-100{
+    width: 100%;
+   
     .container{
         // background-color: #122439;
         height: 50px;
         color: grey;
-        .p-2{
+            .p-2{
             width: 100%;
             ul{
             list-style-type: none;
@@ -85,12 +213,15 @@ export default {
             text-decoration: none;
             color:gray;
     
-            }
+                }
     
-        }
+            }
+
+        }   
     }
-        
-            
+    
+        .color-top{
+            background-color: #122439;
         }
         .green{
             color: #1c918e;
@@ -113,30 +244,67 @@ export default {
         
     .jumbo{
         
-        // width: 100%;
-        // height: 100%;
-        position: relative;
-        background-color: yellow;
-        .jumbo-img{
-            width: 100%;
-            position: absolute;
-        //    left: 0;
-        right: 200px;
-           top: -97px;
+        background-image: url("../assets/img/bg-parallax.png");
+        background-position: center;
+        width: 100%;
+            height: 700px;
+        // background-color: yellow;
+        .jumbo-img{ 
            overflow: hidden;
            img{
-            width: 183em;
+            width: 100%;
             height: 100%;
             }
             
         }
-        .banner-jumbo{
-                position: absolute;
-                color: grey;
-                left: 1100px;
-                top: 310px;
+        .banner-jumbo{  
+            color: grey;
+            margin-top: 10em;
+            
             }
         
+    }
+    .header-bottom{
+        height: 70em;
+        background-color: #EEF1ED;
+        
+        .business{
+            background-color: white;
+            height: 280px;
+            width: 300px;
+            margin-top: 50px;
+            border-radius: 8%;
+            .arrow{
+                position: relative;
+                text-align: center;
+                
+                // background-color: #52bbba;
+                vertical-align: middle;
+                
+                i{
+                    position: absolute;
+                    top: 2px;
+                    left: 0;
+                    padding: 1px;
+                    
+                    
+                    // display: block;
+                    color: #52bbba;
+                    opacity: 0.8;
+                    
+                }
+                
+            }
+            .arrow:hover i{
+                
+                color: #038484;
+                background-color: #52bbba;
+                display: block;
+                border-radius: 50%;
+                width: 30px;
+                height: 30px;
+            }
+        }
     }
     
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,109 @@
 <template>
-  <div></div>
+  <div class="container-fluid">
+    <div class="row row-cols-2 d-flex  justify-content-evenly" >
+        <div class="col-6 text-start ">
+            <div class="company ">
+                <h5 class="fs-6 mb-5">ABOUT THE NETWORK</h5>
+                <h2 class="fs-1"><span class="the">The</span>  Company</h2>
+                <p>For 12 years we have been providing audit and warranty, financial advice, risk advice, taxes and related services to select clients</p>
+                <div class="row row-cols-2 d-flex " >
+                    <div class="col text-start">
+                        <i class="fas fa-award fs-5"> </i><span class="fs-4 ms-3">Tradition</span>
+                        <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                    </div>
+                    <div class="col text-start">
+                        <i class="fas fa-unlock-alt fs-5"> </i><span class="fs-4 ms-3">Security</span>
+                        <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="row row-cols-2 d-flex " >
+                    <div class="col text-start">
+                        <i class="far fa-edit fs-5"></i> <span class="fs-4 ms-3">Certificate</span>
+                        <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                    </div>
+                    <div class="col text-start">
+                        <i class="fas fa-graduation-cap fs-5"> </i><span class="fs-4 ms-3">Expertise</span>
+                        <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                
+                
+                <button class="btn btn-outline-secondary ms-3 " type="submit">GET IN TOUCH</button>
+                <button type="button" class="btn btn-outline-success  green ms-3">READ MORE</button>
+                
+            </div>
+        </div>
+        <div class="col-6 ">
+            <div class="img-company">
+                <img src="../assets/img/about-4.jpg" alt="">
+            </div>
+            
+            
+        </div>
+    </div>
+    
+    <div class="container-main">
+        <p> WE DO MORE FOR EVERYONE</p>
+        <h2>Actions & <span>Projects</span></h2>
+        <div class="menu-main">
+            <nav>
+                <ul>
+                    <li><a class="active" href="http://">ALL</a></li>
+                    <li><a href="http://"></a>INSTITUTIONAL</li>
+                    <li><a href="http://">SOCIAL</a> </li>
+                    <li><a href="http://">EVENTS</a> </li>
+                    <li><a href="http://">INNOVATION</a> </li>
+                    <li><a href="http://">ENVIRONMENT</a> </li>
+                    <li><a href="http://">TECHNOLOGY</a> </li>
+                </ul>      
+            </nav>
+        </div>
+        <div class="action">
+            <div class="d-flex row-cols-3 pb-5" >
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-3.jpg" alt="">
+                    </div>
+                </div>
+            
+            </div>
+            <div class="d-flex row-cols-3 pb-5" >
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-4.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-5.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="project pe-5">
+                        <img src="../assets/img/project-6.jpg" alt="">
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+        
+        
+
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +112,84 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.container-fluid{
+    // background-image: url("../assets/img/image.png");
+    
+    width: 100%;
+    // padding-top: 3em;
+    // margin-top: 5em;
+    .row{
+        background-color: black;
+    }
+    .company{
+        color: white;
+        width: 60%;
+        margin-left: 40%;
+        margin-top: 10em;
+        h5{
+           color: #8fff; 
+        }
+        .the{
+            background-color: #1c918e;
+            border-radius: 10%;
+            opacity: 0.8;
+        }
+        i{
+            color: #1c918e;
+        }
+    }
+
+
+
+    .img-company{
+        overflow: hidden;
+    }
+    .container-main{
+        width: 70%;
+        margin: 0 auto;
+        text-align: center;
+        .menu-main{
+            nav{
+                color: grey;
+                ul{
+                    list-style-type: none;
+                    
+                    li{
+                        display: inline-block;
+                        a{
+
+                            padding: 0.5em 0.5em;
+                            margin-left: 3%;
+                            text-decoration: none;
+                            color:gray;
+    
+                        }
+
+                    } 
+    
+                }     
+                
+            } 
+            nav ul:hover a.active{
+                background-color: rgb(202, 201, 201);
+                color: black;
+            }
+        }
+        .action{
+            background-color: white;
+            .project{
+                width: 100%;
+                flex-wrap: wrap;
+                
+                img{
+                    border-radius: 5%;
+                    width: 100%;
+                }
+
+            }
+        }
+    }
+}
 
 </style>
