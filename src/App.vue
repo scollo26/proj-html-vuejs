be9bfbf1d50e1e010bc3b688224545338bf74241
scollo26 <giovanniscollo26@gmail.com>
2022-01-24 10:44:59 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 export default {
   name: "App",
+  data() {
+    return {
+      
+    }
+  },
   components: {
-    HelloWorld,
+    Header,
+    Main,
+    Footer,
+  },
+  methods: {
+  
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "~bootstrap/scss/bootstrap";
+  // @import '~mdb-ui-kit/css/mdb.min.css';
 </style>
